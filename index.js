67061e086010c3dff22285395216611da1f54a21
Sarva <sarvajitgupta@gmail.com>
2023-12-26 19:20:39 +0530

    Added some changes to package

--- index.js
+++ index.js
@@ -8,15 +8,18 @@ const connectDB = require('./config/db')
 const userRoutes = require('./routes/userRoutes')
 const blogRoutes = require('./routes/blogRoutes')
 dotenv.config();
-
+const path = require('path');
 const app=express();
 connectDB();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
-
+app.use(express.static(path.join(__dirname,'./client/build')))
 app.use("/api",userRoutes)
 app.use("/api/blog",blogRoutes)
+app.use('*',function(req,res){
+    res.sendFile(path.join(__dirname,'./client/build/index.html'));
+})
 const PORT = process.env.PORT || 8000
 app.listen(PORT,()=>{
     console.log(`Server Running on port no. ${PORT}`.bgCyan.white);
